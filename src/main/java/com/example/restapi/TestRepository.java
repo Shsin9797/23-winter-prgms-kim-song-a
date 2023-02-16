43e5156b6d8079a56cd78249799d8a15f7911729
Shsin9797 <aaa8141@naver.com>
2023-02-17 02:58:39 +0900

    T - Repository & Service 구현

--- src/main/java/com/example/restapi/TestRepository.java
+++ src/main/java/com/example/restapi/TestRepository.java
@@ -2,11 +2,19 @@ package com.example.restapi;
 
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class TestRepository { //저장소(DB)랑 대화하는 역할
+import java.util.HashMap;
+import java.util.Map;
 
-    String Repo(String a){
-        return a + " 입니다.";
+@Repository
+public class TestRepository { //저장소(DB)랑 대화해서, 서비스가 원하는 데이터를 주거나 /지우거나 /수정하거나  => CRUD 대신 해주는 친구
+
+   //private Map<String ,String > db =new HashMap<>();
+    //자바 (자료구조) 컬렉션 ... 중 실무는 2개만 알면 됨
+    //list , map(hashmap)
+
+    public String test() { // 나 레파지토리인데.. 내가 갖고잇는 test() 메시지 부르면.. 그값 찾아서 돌려줄게
+        //DB 에서 데이터 select 해와서 return
+        return "test";
     }
 
 }
--- src/main/java/com/example/restapi/TestService.java
+++ src/main/java/com/example/restapi/TestService.java
@@ -11,6 +11,10 @@ public class TestService {
     TestRepository tr;
 
     String test(){
-        return tr.Repo("test");
+        //서비스가 로직짜는 애라고 했는데
+        // return "test"를 repository 가 대신해주면
+        // 여기서 서비스는 지금 이 로직상 repository 에게 일을 시켜서 받기만 하면됨
+
+        return 레포지토리.test();
     }
 }
