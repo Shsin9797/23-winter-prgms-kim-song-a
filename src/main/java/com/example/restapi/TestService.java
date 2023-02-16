a0df5f24f73c4bcfaec940b71a4396b1224b57e7
Shsin9797 <aaa8141@naver.com>
2023-02-17 02:59:22 +0900

    T -  Service 에게 일을 줘보자

--- src/main/java/com/example/restapi/TestService.java
+++ src/main/java/com/example/restapi/TestService.java
@@ -15,6 +15,9 @@ public class TestService {
         // return "test"를 repository 가 대신해주면
         // 여기서 서비스는 지금 이 로직상 repository 에게 일을 시켜서 받기만 하면됨
 
-        return 레포지토리.test();
+        int number = 0;
+        number += 3;
+
+        return 레포지토리.test(number);
     }
 }
